697e89d1b0f86583fbba4c12eb63f47861db0a2b
Tom Sherman <the.tomsherman@gmail.com>
2019-06-25 13:00:05 +0100

    Add logo

--- components/App.js
+++ components/App.js
@@ -7,6 +7,20 @@ import { filterRefer, filterFraud, filterGenuine } from '../util'
 
 const { Header, Content, Sider } = Layout
 
+const Logo = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 829.15 337.32"
+    style={{ width: '2em', marginRight: '1em' }}
+  >
+    <defs />
+    <polygon
+      fill="#fff"
+      points="720.66 0 682.28 113.49 151.05 113.49 112.66 0 0 0 74.92 213.69 352.76 213.69 414.58 337.32 476.39 213.69 754.18 213.69 829.15 0 720.66 0"
+    />
+  </svg>
+)
+
 export const App = withRouter(({ router, children }) => {
   const [{ cases }] = useAppState()
 
@@ -16,9 +30,10 @@ export const App = withRouter(({ router, children }) => {
         <style jsx>{`
           h1 {
             color: white;
+            display: inline-block;
           }
         `}</style>
-        <div className="logo" />
+        <Logo />
         <h1>Rainbird Fraud Detection</h1>
       </Header>
       <Layout className="ant-layout-has-sider">
